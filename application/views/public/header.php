f3ea8008db01c3f1883bb538999b86aac3e6c533
franciscobarrerarodriguez <francisco.barrera@uptc.edu.co>
2017-02-20 16:30:09 -0500

    First

---
+++ application/views/public/header.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <link rel="shortcut icon" href="<?php echo base_url() ?>assets/public/img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo base_url() ?>assets/public/img/favicon.ico" type="image/x-icon">
+
+<!-- Public style -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/public/css/style.css" media="screen" title="no title">
+
+  <!-- Font-awesome.min.css, icon pack -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/font-awesome-4.7.0/css/font-awesome.min.css" media="screen" title="no title">
+
+  <!-- aos.css, animate On Scroll Library. https://michalsnik.github.io/aos/   -->
+    <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+
+    <title></title>
+
+  </head>
+  <body>
+
+  </body>
+  <!-- jquery-3.1.1.min.js -->
+  <script src="<?php echo base_url() ?>assets/sigma/js/jquery-3.1.1.min.js" charset="utf-8"></script>
+  <!-- aos.js, animate On Scroll Library. https://michalsnik.github.io/aos/ -->
+  <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+
+</html>
